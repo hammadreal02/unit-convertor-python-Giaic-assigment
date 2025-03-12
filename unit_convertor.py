a5a5f5e4289f62cd8140bfcf70cd362381cb43b0
hammadreal02 <muhammadhammadhammad588@gmail.com>
2025-03-12 08:15:34 +0500

    add unit converter

--- unit_convertor.py
+++ unit_convertor.py
@@ -8,13 +8,13 @@ st.title("Unit Convertor")
 st.markdown("### Welcome!🥰 to Unit Convertor app ")
 st.write("This is a unit convertor app that converts units from one unit to another.")
 
-#category select
-category = st.selectbox("Select a Category",["📏length","⌚Time","⚖️ Weight","🌡Temperature",'🧪 Volume',"⚖️ Mass","🏃‍♂️ Speed"] )
+# category select 
+category = st.selectbox("Select a Category",["📏length","⌚Time","⚖️ Weight and Mass","🌡Temperature",'🧪 Volume',"🏃‍♂️ Speed"] )
 
 # input numbers for convert
-value = st.number_input("Enter a number to convert", value=0 , min_value=0)
+value = st.number_input("Enter a number to convert", value=1 , min_value=0)
 
-# length category
+#category selected length
 if category == "📏length":
     units ={
         "Meter": 1,
@@ -26,14 +26,16 @@ if category == "📏length":
         "Foot": 0.3048,
         "Inch": 0.0254
     }
-    
+    # category of from unit and To unit 
     from_units = st.selectbox("FROM UNIT",list(units.keys()))
     To_units = st.selectbox("TO UNIT",list(units.keys()))
 
+    # user value *  from units value and divided to To units 
     result = value * units[from_units] / units[To_units]
+    
     st.success(f'{value} {from_units} = {result} {To_units}')
 
-# if  category selected Time
+#  category selected Time
 elif category == "⌚Time":
     units = {
         "Seconds": 1,
@@ -50,8 +52,9 @@ elif category == "⌚Time":
     st.success(f'{value} {from_units} = {result} {To_units}')
 
 #if category selected weigth 
-elif category == "⚖️ Weight":
+elif category == "⚖️ Weight and Mass":
     units ={
+        "Tonne":1000,
         "Kilogram":1,
         "Gram":0.001,
         "Pound":0.453592,
@@ -66,40 +69,27 @@ elif category == "⚖️ Weight":
 elif category == "🧪 Volume":
     units ={
         "litre":1,
-        "Millilitre":0.001,
+        "Millilitre":0.001,         #1 millilitr = 0.001 litr
         "Gallon": 3.78541,
-        "Cubic Meter": 1000.0
+        "Cubic Meter": 1000.0,
     }
     from_units = st.selectbox("FROM UNIT",list(units.keys()))
     To_units = st.selectbox("TO UNIT",list(units.keys()))
     result = value * units[from_units] / units[To_units]
     st.success(f'{value} {from_units} = {result} {To_units}')
 
-
-#if category selected mass
-elif category == "⚖️ Mass":
-    units={
-        "Kilogram":1,
-        "Gram":0.001,
-        "Pound":0.453592,
-        "Ounce":0.0283495
-    }  
-    from_units = st.selectbox("From",list(units.keys()))
-    To_units = st.selectbox("TO",list(units.keys()))
-    result = value * units[from_units] / units[To_units]
-    st.success(f'{value} {from_units} = {result} {To_units}')  
 # if category selected speed  
-elif category == "🚀 speed":
+elif category == "🏃‍♂️ Speed":
     units={ 
-        "meter per second m/s": 1.0,
-        "kilometer per hour Km/h": 1000.0 / 3600.0,  # 1km/h = 0.27778
+        "meter per second ": 1.0,
+        "kilometer per hour ": 1000.0 / 3600.0,      # 1km/h = 0.27778
         "feet per hour": 0.3048 /3600.0,             # 1ft/h = 0.0000846667 m/s
         "miles per hour": 1609.344 / 3600.0,         # 1mp/h =  0.44704 m/s
     }
     from_units = st.selectbox("FROM",list(units.keys()))
     To_units = st.selectbox("TO",list(units.keys()))
     result = value * units[from_units] / units[To_units]
-    st.success(f'{value} {from_units} = {result:.4f} {To_units}')
+    st.success(f'{value} {from_units} = {result:.6f} {To_units}')
 
 #if category selected Temperture
 elif category == "🌡Temperature":
@@ -110,21 +100,27 @@ elif category == "🌡Temperature":
     # con..   is from units and  To units same than output is value
     if from_units == To_units:
         result = value
+
     #con..  is celsius to fahrenheit 
     elif from_units == "Celsius" and To_units == "Fahrenheit":
         result =(value * 9/5) + 32
+
     #con..  is celsius to kelvin
     elif from_units == "Celsius" and To_units == "kelvin":
         result = value + 273.15
+
     #con..  is fahrenheit to celsius
     elif from_units == "Fahrenheit" and To_units == "Celsius":
         result = (value - 32) * 5/9
+
     #con..  is fahrenheit to kelvin
     elif from_units == "Fahrenheit" and To_units == "kelvin":
         result = (value - 32) * 5/9 + 273.15
+
     #con..  is kelvin to Celsius
     elif from_units == "kelvin" and To_units == "Celsius":
         result = value - 273.15
+
     #con..  is kelvin to fahrenheit
     elif from_units == "kelvin" and To_units == "Fahrenheit":
         result = (value - 273.15) *9/5 + 32
@@ -134,4 +130,4 @@ elif category == "🌡Temperature":
     # ex: category select temperture and value is 120 and select from unit is kelvin and To units is fahrenheit.
     # than success message is "120 kelvin =  -53.083 fahrenheit" 
     # (result:.3f)means after . three number 
-    st.success(f'{value} {from_units} = {result:.3f} {To_units}')    
+    st.success(f'{value} {from_units} =  {result:.5f} {To_units}')    
